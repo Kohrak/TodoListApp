bef7175b85f12b1dfbd56b132b027f7be77933d2
Kohrak <gabrielgt95@gmail.com>
2018-03-10 19:14:22 +0000

    add append new tasks functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("done");
 })
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   event.stopPropagation();
   $(this).parent().fadeOut(200, function(){
     $(this).remove();
   });
+})
+
+$("input[type='text']").keypress(function(event){
+  if (event.which === 13){
+    var todotxt = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X </span>" + todotxt + "</li>");
+  }
 })
