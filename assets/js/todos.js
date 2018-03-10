a6753512a9528ae86e7855169c074a1a88f616a4
Kohrak <gabrielgt95@gmail.com>
2018-03-10 19:03:07 +0000

    add fadeout and remove task functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,10 @@
 $("li").click(function(){
   $(this).toggleClass("done");
 })
+
+$("span").click(function(event){
+  event.stopPropagation();
+  $(this).parent().fadeOut(200, function(){
+    $(this).remove();
+  });
+})
