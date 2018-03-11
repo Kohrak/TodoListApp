8873381d852bf112b9f3e00a9ad06421b795e90d
Kohrak <gabrielgt95@gmail.com>
2018-03-11 11:47:18 +0000

    add toggle input funtionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -13,6 +13,10 @@ $("input[type='text']").keypress(function(event){
   if (event.which === 13){
     var todotxt = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X </span>" + todotxt + "</li>");
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span>" + todotxt + "</li>");
   }
+})
+
+$("#toggle-form").click(function(){
+  $("input[type='text']").toggle();
 })
